df58fad34d72bda4747ea92889d209b12abe799d
ehddnr301 <dy950328@gmail.com>
2020-05-09 20:33:07 +0900

    userInfoPage

--- src/Routes/Profile.tsx
+++ src/Routes/Profile.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
 import { connect } from "react-redux";
+import Loading from "../Components/Loading";
 
 const Container = styled.div`
   width: 100vw;
@@ -10,22 +11,47 @@ const Container = styled.div`
 
 const UserDetail = styled.div``;
 
+const ProfileImage = styled.img``;
+
+const Username = styled.div``;
+
+const Email = styled.div``;
+
+interface IUserInfo {
+  email: string;
+  username: string;
+  avatarUrl: string;
+}
+
 const Profile = (props): JSX.Element => {
   let userId = "";
+  const [userInfo, setUserInfo] = useState<IUserInfo | undefined>(undefined);
   if (props.state.length === 1) {
     userId = props.state[0].userInfo.id;
   }
 
   useEffect(() => {
     async function getUserInfo() {
-      const userInfo = await axios.get(`http://localhost:4000/user/${userId}`);
-      console.log(userInfo);
+      const {
+        data: { email, username, avatarUrl },
+      } = await axios.get(`http://localhost:4000/user/${userId}`);
+      setUserInfo({ email, username, avatarUrl });
     }
     getUserInfo();
   });
   return (
     <Container>
-      <UserDetail></UserDetail>
+      {userInfo ? (
+        userInfo && (
+          <UserDetail>
+            <ProfileImage src={userInfo.avatarUrl}></ProfileImage>
+            <Username>{userInfo.username}</Username>
+            <Email>{userInfo.email}</Email>
+          </UserDetail>
+        )
+      ) : (
+        <Loading></Loading>
+      )}
     </Container>
   );
 };
