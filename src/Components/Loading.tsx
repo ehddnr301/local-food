988362a75aa4972ac2fd65b7ae0699de6be31b76
ehddnr301 <dy950328@gmail.com>
2020-06-06 15:34:33 +0900

    loading

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -1,3 +1,26 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
 
-export default () => <div>loading</div>;
+const rotate = keyframes` {
+    from {
+        transform: rotate(0deg);
+    }
+    to { 
+        transform: rotate(360deg);
+    }
+}
+`;
+
+const Loading = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 110px auto 0;
+  border: solid 10px #8822aa;
+  border-radius: 50%;
+  border-right-color: transparent;
+  border-bottom-color: transparent;
+  transition: all 0.5s ease-in;
+  animation: ${rotate} 1s linear infinite;
+`;
+
+export default () => <Loading></Loading>;
