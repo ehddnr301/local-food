f9a6355219d5fc902579c9503c73981a922a2b57
ehddnr301 <dy950328@gmail.com>
2020-06-08 23:01:41 +0900

    profile

--- src/Routes/Profile.tsx
+++ src/Routes/Profile.tsx
@@ -16,21 +16,56 @@ const Container = styled.div`
 
 const UserDetail = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
-  margin-top: 10%;
   width: 60%;
   height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  font-size: 18px;
+`;
+
+const UserStore = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(200, 30, 30, 0.4);
 `;
 
 const ProfileImage = styled.img`
-  width: 15%;
+  width: 27%;
   height: 17%;
+  border-radius: 50%;
 `;
 
-const Username = styled.div``;
+const Username = styled.div`
+  margin-top: 20px;
+`;
 
 const Email = styled.div``;
+
+const Logout = styled.button`
+  margin-top: 10px;
+  background-color: white;
+  border-radius: 10px;
+  border: 1px solid #2ecc71;
+  color: #2ecc71;
+  text-transform: uppercase;
+  width: 100px;
+  transition: background-color 0.5s ease, color 1s ease;
+  cursor: pointer;
+  box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.73);
+  &:hover {
+    background-color: #2ecc71;
+    color: white;
+  }
+`;
 
 interface IUserInfo {
   email: string;
@@ -74,10 +109,13 @@ const Profile = ({ history, dispatch }): JSX.Element => {
         userInfo ? (
           userInfo && (
             <UserDetail>
-              <ProfileImage src={userInfo.avatarUrl}></ProfileImage>
-              <Username>{userInfo.username}</Username>
-              <Email>{userInfo.email}</Email>
-              <button onClick={onClick}>logout</button>
+              <UserInfo>
+                <ProfileImage src={userInfo.avatarUrl}></ProfileImage>
+                <Username>username : {userInfo.username}</Username>
+                <Email>{userInfo.email}</Email>
+                <Logout onClick={onClick}>logout</Logout>
+              </UserInfo>
+              <UserStore></UserStore>
             </UserDetail>
           )
         ) : (
